a85a2a0b4c2ebb63ae7c32afda82416c8b2d7689
Ale0810 <alejandroverteche@gmail.com>
2025-01-19 13:23:32 -0300

    Estrucura de pagina principal mejorada con sidebar responsiva

--- SistemaFinanzas/src/Components/Sidebar/Sidebar.jsx
+++ SistemaFinanzas/src/Components/Sidebar/Sidebar.jsx
@@ -1,13 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
-function Sidebar({ logo, links, children }) {
-    const [isOpen, setIsOpen] = useState(false);
+function Sidebar({ logo, links, isOpen, toggleSidebar }) {
     const navigate = useNavigate();
-
-    const toggleSidebar = () => {
-        setIsOpen(!isOpen);
-    };
 
     const cerrarSesion = () => {
         localStorage.removeItem("token");
@@ -15,12 +10,11 @@ function Sidebar({ logo, links, children }) {
     };
 
     return (
-        <>
+        <div>
+            {/* Overlay */}
+            <div className={`fixed inset-0 bg-black opacity-50 z-30 ${isOpen ? 'block' : 'hidden'}`} onClick={toggleSidebar} />
             {/* Sidebar */}
-            <div
-                className={`bg-gray-800 text-white w-64 transform transition-transform duration-300 ${isOpen ? "translate-x-0" : "-translate-x-full"
-                    } fixed inset-y-0 left-0 z-40 lg:translate-x-0`}
-            >
+            <div className={`fixed h-full flex flex-col bg-gray-800 text-white w-64 transform transition-transform duration-300 inset-y-0 left-0 z-40 ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 md:static md:inset-0`}>
                 {/* Logo */}
                 <div className="p-4 text-xl font-bold border-b border-gray-700">
                     {logo}
@@ -31,28 +25,20 @@ function Sidebar({ logo, links, children }) {
                         <a
                             key={index}
                             href={link.path}
-                            className="block py-2 px-4 rounded hover:bg-gray-700"
-                        >
+                            className="block py-2 px-4 rounded hover:bg-gray-700">
                             {link.text}
                         </a>
                     ))}
                 </nav>
+                <p className="border-b border-gray-700" ></p>
                 {/* Footer */}
                 <div className="flex justify-center p-4">
-                    <button
-                        onClick={cerrarSesion}
-                        className="block text-sm text-gray-400 hover:underline"
-                    >
+                    <button onClick={cerrarSesion} className="block text-sm text-gray-400 hover:underline">
                         Cerrar sesión
                     </button>
                 </div>
             </div>
-
-            <button
-                className="lg:hidden p-4 text-gray-800 bg-gray-200 fixed top-4 left-4 z-50 rounded focus:outline-none focus:ring" onClick={toggleSidebar}>
-                ☰
-            </button>
-        </>
+        </div>
     );
 }
 
--- SistemaFinanzas/src/Pages/Principal.jsx
+++ SistemaFinanzas/src/Pages/Principal.jsx
@@ -10,29 +10,35 @@ const Principal = () => {
         { text: "Reportes", path: "/reportes" },
     ]);
 
+    const [sidebarOpen, setSidebarOpen] = useState(false);
     return (
         <>
-            <div className="flex h-full w-full">
-                <Sidebar logo={"Gestión Finanzas Personales"} links={links} />
-                <div className="flex flex-col flex-grow items-center justify-center bg-white">
-                    <h1 className="text-4xl font-bold text-gray-800">Bienvenido</h1>
-                    <p className="text-gray-500">Selecciona una opción del menú</p>
-                    <div className="flex flex-wrap justify-center items-center gap-4 mt-4">
-                        <div className="bg-blue-500 text-white rounded p-4 text-center">
-                            <p className="text-2xl font-bold">Cuentas</p>
-                            <p>Administra tus cuentas</p>
-                        </div>
-                        <div className="bg-green-500 text-white rounded p-4 text-center">
-                            <p className="text-2xl font-bold">Movimientos</p>
-                            <p>Registra tus movimientos</p>
-                        </div>
-                        <div className="bg-yellow-500 text-white rounded p-4 text-center">
-                            <p className="text-2xl font-bold">Reportes</p>
-                            <p>Genera reportes de tus finanzas</p>
-                        </div>
-                        <div className="bg-red-500 text-white rounded p-4 text-center">
-                            <p className="text-2xl font-bold">Perfil</p>
-                            <p>Actualiza tu perfil</p>
+            <div className="flex items-center justify-center h-[93vh] w-full ">
+                <div className="relative flex h-[90vh] w-[80vw] rounded-3xl shadow-lg overflow-hidden">
+                    <Sidebar logo={"Gestión Finanzas Personales"} links={links} isOpen={sidebarOpen} toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
+                    <div className="flex-1 flex flex-col items-center justify-center bg-white p-10">
+                        <button className="absolute top-4 left-4 p-3 rounded-lg bg-gray-800 text-white md:hidden" onClick={() => setSidebarOpen(!sidebarOpen)} >
+                            &#9776; {/* Ícono hamburguesa */}
+                        </button>
+                        <h1 className="text-4xl font-bold text-gray-800">Bienvenido</h1>
+                        <p className="text-gray-500">Selecciona una opción del menú</p>
+                        <div className="flex flex-wrap justify-center items-center gap-4 mt-4">
+                            <div className="bg-blue-500 text-white rounded p-4 text-center">
+                                <p className="text-2xl font-bold">Cuentas</p>
+                                <p>Administra tus cuentas</p>
+                            </div>
+                            <div className="bg-green-500 text-white rounded p-4 text-center">
+                                <p className="text-2xl font-bold">Movimientos</p>
+                                <p>Registra tus movimientos</p>
+                            </div>
+                            <div className="bg-yellow-500 text-white rounded p-4 text-center">
+                                <p className="text-2xl font-bold">Reportes</p>
+                                <p>Genera reportes de tus finanzas</p>
+                            </div>
+                            <div className="bg-red-500 text-white rounded p-4 text-center">
+                                <p className="text-2xl font-bold">Perfil</p>
+                                <p>Actualiza tu perfil</p>
+                            </div>
                         </div>
                     </div>
                 </div>
